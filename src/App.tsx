cd5a656235251a2b626b7775c5bdf2a2e6624132
Auguste-Houndjago <augustehoundjago@gmail.com>
2024-12-18 09:20:48 +0000

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import ContactPage from './pages/ContactPage';
+import Blog from './pages/Blog';
+import Messages from './pages/Messages';
 
 function App() {
-  return <ContactPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/messages" element={<Messages />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
